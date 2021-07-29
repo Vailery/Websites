8f16e12235e57027b63519afb06782b917a9c667
Vailery <leraliabik8@gmail.com>
2021-07-29 16:00:53 +0300

    feat: add video popup
    add video with styles and code for show it

--- travel-landing-page/assets/js/main.js
+++ travel-landing-page/assets/js/main.js
@@ -17,4 +17,19 @@ showMenu('nav-toggle', 'nav-menu')
 
 // Popup
 
+const btnOpenVideo = document.querySelectorAll('.islands__video-content')
+const islandsPopup = document.getElementById('popup')
+
+function poPup() {
+    islandsPopup.classList.add('show-popup')
+}
+
+btnOpenVideo.forEach(b => b.addEventListener('click', poPup))
+
+const btnCloseVideo = document.getElementById('popup-close')
+
+btnCloseVideo.addEventListener('click', () => {
+    islandsPopup.classList.remove('show-popup')
+})
+
 // GSAP animation
